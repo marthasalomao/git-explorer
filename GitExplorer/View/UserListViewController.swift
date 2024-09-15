0af17493706abed3f91280d1db9855e2871a317d
marthasalomao <marthamsalomao@gmail.com>
2024-09-15 03:39:56 -0300

    Initial Commit

---
+++ GitExplorer/View/UserListViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Users"
+    }
+
+
+}
+
